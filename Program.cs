474feed88ba8b9b6c2d485e5c8577b358bfc3451
Greg Smith <gregsmith446@gmail.com>
2019-03-14 11:39:27 -0400

    completed codeSignal #2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CenturyFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // to test other years, 
+            int result = CenturyFromYear(1700);
+            Console.WriteLine(result);
+        }
+        static int CenturyFromYear(int year)
+        {
+            if (year % 100 == 0)
+            {
+                return (year / 100);
+            }
+            else
+            {
+                return ((year / 100) + 1);
+            }
+        }
+    }
+}
